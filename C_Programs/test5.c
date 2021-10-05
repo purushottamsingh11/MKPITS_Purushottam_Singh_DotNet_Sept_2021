c712457c7d759bb618783146644529cfbdd3d447
purushottamsingh11 <91671119+purushottamsingh11@users.noreply.github.com>
2021-10-05 08:48:11 +0530

    Add files via upload

---
+++ C_Programs/test5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a=100,multi,div;
+int b=50,sub;
+float c=2.5,sum;
+sum=a+c;
+sub=b-a;
+multi=a*b;
+printf("\n addition of a and c=%f \n sub of b and a=%d",sum,sub);
+printf("\n mul of a and b is %d",multi);
+return 0;
+
+}
+
---
+++ C_Programs/test7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int r;
+float pi=3.14,area;
+
+printf("\n enter the value of radius :");
+scanf("%d",&r);
+
+area=pi*r*r;
+
+printf("\n value of area=%f",area);
+
+return 0;
+}
+

8085a9a345192db6c2364307044590744f84d06c
purushottamsingh11 <91671119+purushottamsingh11@users.noreply.github.com>
2021-10-23 09:45:52 +0530

    Update sum of each row of array 2 cross 3  size.c

--- C_Program/ASSIGNMENT/12 october/sum of each row of array 2 cross 3  size.c
+++ C_Program/ASSIGNMENT/12 october/sum of each row of array 2 cross 3  size.c
@@ -1,17 +1,16 @@
-	 #include<stdio.h>
-  	 int main()
-	 {
- 	 int i,j,sum1=0,sum2=0,a[2][3];
+	      #include<stdio.h>
+  	      int main()
+	      {
+ 	        int i,j,sum1=0,sum2=0,a[2][3];
 		printf("enter 6 element for 2*3 array=");
-
-		for(i=0;i<2;i++)
+                for(i=0;i<2;i++)
 		 {
   			for(j=0;j<3;j++)
   			{
   			 scanf("%d",&a[i][j]);
   			}
  		 }
-        printf("\n array size 2*3 is:\n");
+                printf("\n array size 2*3 is:\n");
 		
 		for(i=0;i<2;i++)
 		 {
@@ -22,7 +21,7 @@
 			 printf("\n");
  		 }
 		printf("\n first row is: ");
-	    for(i=0;i<1;i++)
+	        for(i=0;i<1;i++)
 		 {
   			for(j=0;j<3;j++)
   			{
@@ -31,14 +30,14 @@
   			}
  		 }
 		 
-	     printf("\n sum of element of first row  is :");
+	         printf("\n sum of element of first row  is :");
 		 printf("%d",sum1);
 		
 		 printf("\n ");
 	  	 printf("\n ");
 		 
 		 printf("\n second row  is: :");
-	     for(i=1;i<2;i++)
+	         for(i=1;i<2;i++)
 		  {
   			for(j=0;j<3;j++)
   			{
@@ -49,7 +48,7 @@
 		 
 		 printf("\n sum of element of second row  is :");
 		 printf("%d",sum2);  
-	   printf("\n************************************** ");
-   return 0;
-  }
+	         printf("\n************************************** ");
+          return 0;
+          }
 

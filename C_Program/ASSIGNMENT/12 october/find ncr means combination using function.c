ae397895c737a80a0abbaf4b779da8827f71938d
purushottamsingh11 <91671119+purushottamsingh11@users.noreply.github.com>
2021-10-25 23:37:47 +0530

    Update find ncr means combination using function.c

--- C_Program/ASSIGNMENT/12 october/find ncr means combination using function.c
+++ C_Program/ASSIGNMENT/12 october/find ncr means combination using function.c
@@ -3,7 +3,7 @@
 	int main()
 	{
         int n,r,ncr;
-  	printf("\n enter n for c to finding ncr and npr=");
+  	printf("\n enter n for c to finding ncr=");
   	scanf("%d%d",&n,&r);
         ncr=comb(n,r);
   	printf("\n combination of %dc%d=%d",n,r,ncr);
@@ -28,6 +28,31 @@
 		    }
 		
 		
+  /*    #include<stdio.h>                      
+	int Comb_Num(int);                        /*  vvvvvvimp styl  */
+	int main()
+	{
+	 int n,r,ncr;
+ 	printf("Enter the number");
+ 	scanf("%d",&n);
+ 	printf("Enter the number");
+ 	scanf("%d",&r);
+ 	ncr = Comb_Num(n)/(Comb_Num(r)*Comb_Num(n-r));      
+ 	printf("Value of %dC%d = %d \n",n,r,ncr);
+	return 0;
+
+	}
+
+	int Comb_Num(int m)
+	{
+    	int i,f=1;
+    	for(i=1;i<=m;i++)
+    	{
+        f=f*i;
+    	}
+    	return f;
+
+	}  */
 
 
 

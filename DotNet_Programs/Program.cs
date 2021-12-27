853d04ce1cfec7ccf8a49ff5be5ef3093211c43b
purushottamsingh11 <91671119+purushottamsingh11@users.noreply.github.com>
2021-12-27 13:51:19 +0530

    Add files via upload

---
+++ DotNet_Programs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Difference_between_STATIC_and_NONSTATIC_function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter salary of employee= ");
+            double salary=Convert.ToDouble(Console.ReadLine());
+        }
+        public static double GrossSalary(double sal)
+        {
+            double Gs=sal+sal*0.3+500;
+            return Gs;  
+        }
+    }
+}
